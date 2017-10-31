9a7894806364d868b25ea8ad6140e9bd9d380db7
MERT\Mert <mertozturk1903@gmail.com>
2017-10-31 17:58:05 +0300

    10/31/2017

---
+++ EntityLayer/Kasa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class Kasa
+    {
+        public int Id { get; set; }
+        public string KasaAdi { get; set; }
+        public string ParaBirimi { get; set; }
+        public DateTime SonIslemTarihi { get; set; }
+    }
+    public class KasaIslemi
+    {
+        public int Id { get; set; }
+        public string IslemTipi { get; set; }
+        public decimal Ucret { get; set; }
+        public string Aciklama { get; set; }
+        public DateTime Tarih { get; set; }
+
+    }
+}
